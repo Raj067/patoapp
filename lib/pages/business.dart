1ed4bc344987777b159c618bae1d041f20f836ba
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-06-12 19:26:52 +0300

    ---

--- lib/pages/business.dart
+++ lib/pages/business.dart
@@ -16,7 +16,7 @@ class BusinessPage extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
         child: ListView(
           children: [
-            const FirstRowBusinessData(),
+            _firstRowBusinessData(context),
             // SecondRowBusinessData(),
             Card(
               child: Container(
@@ -53,154 +53,267 @@ class BusinessPage extends StatelessWidget {
       ),
     );
   }
-}
 
-class FirstRowBusinessData extends StatelessWidget {
-  const FirstRowBusinessData({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
+  _firstRowBusinessData(BuildContext context) {
     return Card(
       child: SizedBox(
         height: 160,
-        child: Column(children: [
-          Container(
-            alignment: AlignmentDirectional.centerStart,
-            height: 60,
-            child: Row(children: [
-              Expanded(
-                  child: Container(
-                height: 60,
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
-                  border: Border(
-                      top: BorderSide(width: 1, color: patoGreen),
-                      left: BorderSide(width: 1, color: patoGreen),
-                      right: BorderSide(width: 1, color: patoGreen),
-                      bottom: BorderSide(width: 1, color: patoGreen)),
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      Text("Sales",
-                          style: TextStyle(color: patoGrey, fontSize: 18)),
-                      Text("Tsh 12,000",
-                          style: TextStyle(color: patoGreen, fontSize: 16))
-                    ]),
-              )),
-              Expanded(
-                  child: Container(
-                height: 80,
-                decoration: const BoxDecoration(
-                  borderRadius:
-                      BorderRadius.only(topRight: Radius.circular(10)),
-                  border: Border(
-                      top: BorderSide(width: 1, color: patoRed),
-                      left: BorderSide(width: 1, color: patoRed),
-                      right: BorderSide(width: 1, color: patoRed),
-                      bottom: BorderSide(width: 1, color: patoRed)),
-                ),
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      Text("Expenses",
-                          style: TextStyle(color: patoGrey, fontSize: 18)),
-                      Text("Tsh 12,000",
-                          style: TextStyle(color: patoRed, fontSize: 16))
-                    ]),
-              )),
-            ]),
-          ),
-          SizedBox(
-            height: 40,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Text(
-                    "Profit",
-                    style: TextStyle(color: patoGrey),
+        child: Column(
+          children: [
+            Container(
+              alignment: AlignmentDirectional.centerStart,
+              height: 60,
+              child: Row(children: [
+                Expanded(
+                    child: Container(
+                  height: 60,
+                  decoration: const BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(10)),
+                    border: Border(
+                        top: BorderSide(width: 1, color: patoGreen),
+                        left: BorderSide(width: 1, color: patoGreen),
+                        right: BorderSide(width: 1, color: patoGreen),
+                        bottom: BorderSide(width: 1, color: patoGreen)),
                   ),
-                  Text(
-                    "Tshs 13,900",
-                    style: TextStyle(color: patoGreen),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text("Sales",
+                            style: TextStyle(color: patoGrey, fontSize: 18)),
+                        Text("Tsh 12,000",
+                            style: TextStyle(color: patoGreen, fontSize: 16))
+                      ]),
+                )),
+                Expanded(
+                    child: Container(
+                  height: 80,
+                  decoration: const BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(topRight: Radius.circular(10)),
+                    border: Border(
+                        top: BorderSide(width: 1, color: patoRed),
+                        left: BorderSide(width: 1, color: patoRed),
+                        right: BorderSide(width: 1, color: patoRed),
+                        bottom: BorderSide(width: 1, color: patoRed)),
                   ),
-                ],
-              ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text("Expenses",
+                            style: TextStyle(color: patoGrey, fontSize: 18)),
+                        Text("Tsh 12,000",
+                            style: TextStyle(color: patoRed, fontSize: 16))
+                      ]),
+                )),
+              ]),
             ),
-          ),
-          const Divider(
-            indent: 10,
-            endIndent: 10,
-          ),
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => FinancialReportDialog(),
-                  fullscreenDialog: true,
-                ),
-              );
-            },
-            child: SizedBox(
+            SizedBox(
               height: 40,
               child: Padding(
                 padding: const EdgeInsets.all(10),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: const [
-                        Icon(Icons.file_copy, color: patoBlue),
-                        Text("Financial Reports",
-                            style: TextStyle(color: patoBlue)),
-                      ],
+                  children: const [
+                    Text(
+                      "Profit",
+                      style: TextStyle(color: patoGrey),
                     ),
-                    const Icon(Icons.arrow_forward_ios, color: patoBlue),
+                    Text(
+                      "Tshs 13,900",
+                      style: TextStyle(color: patoGreen),
+                    ),
                   ],
                 ),
               ),
             ),
-          ),
-
-          // const SizedBox(
-          //   height: 40,
-          //   child: ListTile(
-          //     title: Text("Profit", style: TextStyle(color: patoGrey)),
-          //     trailing: Text("Tshs 13,900", style: TextStyle(color: patoGreen)),
-          //   ),
-          // ),
-          // const Divider(
-          //   indent: 10,
-          //   endIndent: 10,
-          // ),
-
-          // SizedBox(
-          //   height: 40,
-          //   child: ListTile(
-          //     // style: ,
-          //     leading: const Icon(Icons.file_copy, color: patoBlue),
-          //     title: const Text("Financial Reports",
-          //         style: TextStyle(color: patoBlue)),
-          //     trailing: const Icon(Icons.arrow_forward_ios, color: patoBlue),
-          //     onTap: () {
-          //       Navigator.push(
-          //         context,
-          //         MaterialPageRoute<void>(
-          //           builder: (BuildContext context) => FinancialReportDialog(),
-          //           fullscreenDialog: true,
-          //         ),
-          //       );
-          //     },
-          //   ),
-          // ),
-        ]),
+            const Divider(
+              indent: 10,
+              endIndent: 10,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => FinancialReportDialog(),
+                    fullscreenDialog: true,
+                  ),
+                );
+              },
+              child: SizedBox(
+                height: 40,
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: const [
+                          Icon(Icons.file_copy, color: patoBlue),
+                          Text("Financial Reports",
+                              style: TextStyle(color: patoBlue)),
+                        ],
+                      ),
+                      const Icon(Icons.arrow_forward_ios, color: patoBlue),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+// class FirstRowBusinessData extends StatelessWidget {
+//   const FirstRowBusinessData({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       child: SizedBox(
+//         height: 160,
+//         child: Column(children: [
+//           Container(
+//             alignment: AlignmentDirectional.centerStart,
+//             height: 60,
+//             child: Row(children: [
+//               Expanded(
+//                   child: Container(
+//                 height: 60,
+//                 decoration: const BoxDecoration(
+//                   borderRadius: BorderRadius.only(topLeft: Radius.circular(10)),
+//                   border: Border(
+//                       top: BorderSide(width: 1, color: patoGreen),
+//                       left: BorderSide(width: 1, color: patoGreen),
+//                       right: BorderSide(width: 1, color: patoGreen),
+//                       bottom: BorderSide(width: 1, color: patoGreen)),
+//                 ),
+//                 child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                     children: const [
+//                       Text("Sales",
+//                           style: TextStyle(color: patoGrey, fontSize: 18)),
+//                       Text("Tsh 12,000",
+//                           style: TextStyle(color: patoGreen, fontSize: 16))
+//                     ]),
+//               )),
+//               Expanded(
+//                   child: Container(
+//                 height: 80,
+//                 decoration: const BoxDecoration(
+//                   borderRadius:
+//                       BorderRadius.only(topRight: Radius.circular(10)),
+//                   border: Border(
+//                       top: BorderSide(width: 1, color: patoRed),
+//                       left: BorderSide(width: 1, color: patoRed),
+//                       right: BorderSide(width: 1, color: patoRed),
+//                       bottom: BorderSide(width: 1, color: patoRed)),
+//                 ),
+//                 child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                     children: const [
+//                       Text("Expenses",
+//                           style: TextStyle(color: patoGrey, fontSize: 18)),
+//                       Text("Tsh 12,000",
+//                           style: TextStyle(color: patoRed, fontSize: 16))
+//                     ]),
+//               )),
+//             ]),
+//           ),
+//           SizedBox(
+//             height: 40,
+//             child: Padding(
+//               padding: const EdgeInsets.all(10),
+//               child: Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: const [
+//                   Text(
+//                     "Profit",
+//                     style: TextStyle(color: patoGrey),
+//                   ),
+//                   Text(
+//                     "Tshs 13,900",
+//                     style: TextStyle(color: patoGreen),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//           const Divider(
+//             indent: 10,
+//             endIndent: 10,
+//           ),
+//           InkWell(
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute<void>(
+//                   builder: (BuildContext context) => FinancialReportDialog(),
+//                   fullscreenDialog: true,
+//                 ),
+//               );
+//             },
+//             child: SizedBox(
+//               height: 40,
+//               child: Padding(
+//                 padding: const EdgeInsets.all(10),
+//                 child: Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Row(
+//                       children: const [
+//                         Icon(Icons.file_copy, color: patoBlue),
+//                         Text("Financial Reports",
+//                             style: TextStyle(color: patoBlue)),
+//                       ],
+//                     ),
+//                     const Icon(Icons.arrow_forward_ios, color: patoBlue),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+
+//           // const SizedBox(
+//           //   height: 40,
+//           //   child: ListTile(
+//           //     title: Text("Profit", style: TextStyle(color: patoGrey)),
+//           //     trailing: Text("Tshs 13,900", style: TextStyle(color: patoGreen)),
+//           //   ),
+//           // ),
+//           // const Divider(
+//           //   indent: 10,
+//           //   endIndent: 10,
+//           // ),
+
+//           // SizedBox(
+//           //   height: 40,
+//           //   child: ListTile(
+//           //     // style: ,
+//           //     leading: const Icon(Icons.file_copy, color: patoBlue),
+//           //     title: const Text("Financial Reports",
+//           //         style: TextStyle(color: patoBlue)),
+//           //     trailing: const Icon(Icons.arrow_forward_ios, color: patoBlue),
+//           //     onTap: () {
+//           //       Navigator.push(
+//           //         context,
+//           //         MaterialPageRoute<void>(
+//           //           builder: (BuildContext context) => FinancialReportDialog(),
+//           //           fullscreenDialog: true,
+//           //         ),
+//           //       );
+//           //     },
+//           //   ),
+//           // ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
 
 class SecondRowBusinessData extends StatelessWidget {
   const SecondRowBusinessData({Key? key}) : super(key: key);
