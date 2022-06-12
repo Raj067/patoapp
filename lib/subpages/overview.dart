8c63cefc576fe3ea32c47fd3d2bc498a6b4bd3d9
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-06-12 19:17:45 +0300

    ---

--- lib/subpages/overview.dart
+++ lib/subpages/overview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:patoapp/components/themeData.dart';
+// import 'package:charts_flutter/flutter.dart' as charts;
 
 class OverviewDialog extends StatelessWidget {
   const OverviewDialog({Key? key}) : super(key: key);
@@ -27,6 +28,7 @@ class OverviewDialog extends StatelessWidget {
         padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
         child: ListView(
           children: [
+            // _barChartOverview(),
             _expenses(),
             _invoices(),
             _payable(),
@@ -41,247 +43,274 @@ class OverviewDialog extends StatelessWidget {
 }
 
 Widget _expenses() => Card(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Expenses",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Expenses",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Paid Invoices"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Unpaid Invoices"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Overdue Invoices"),
                 Text("TZS 80,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Balance"),
                 Text("TZS 120,000"),
-              ])
-        ],
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
 
 Widget _invoices() => Card(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Invoices",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Invoices",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Paid Invoices"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Unpaid Invoices"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Overdue Invoices"),
                 Text("TZS 80,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Balance"),
                 Text("TZS 120,000"),
-              ])
-        ],
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
 
 Widget _payable() => Card(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Payables",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Payables",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("To Customers"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("To Suppliers"),
                 Text("TZS 120,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Balance"),
                 Text("TZS 120,000"),
-              ])
-        ],
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
 
 Widget _receivable() => Card(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Receivable",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Receivable",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("From Customers"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("From Suppliers"),
                 Text("TZS 120,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Balance"),
                 Text("TZS 120,000"),
-              ])
-        ],
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
 
 Widget _profitLoss() => Card(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Profit & Loss",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Profit & Loss",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Sales"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("COGS"),
                 Text("TZS 120,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Gross Profit"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Expenses"),
                 Text("TZS 120,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Netprofit"),
                 Text("TZS 120,000"),
-              ])
-        ],
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
 
 Widget _balanceSheet() => Card(
-        child: Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Balance Sheet",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 5),
-          const Text(
-            "Balance Sheet as at 07-06-2022",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-          ),
-          Container(height: 10),
-          const Text(
-            "Assets",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Balance Sheet",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 5),
+            const Text(
+              "Balance Sheet as at 07-06-2022",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+            ),
+            Container(height: 10),
+            const Text(
+              "Assets",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Current Asset"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Receivable"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Fixed Assets"),
                 Text("TZS 120,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text(
@@ -292,33 +321,37 @@ Widget _balanceSheet() => Card(
                   "TZS 120,000",
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              ]),
-          Container(height: 10),
-          const Text(
-            "Liabilities",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          Container(height: 10),
-          Row(
+              ],
+            ),
+            Container(height: 10),
+            const Text(
+              "Liabilities",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Container(height: 10),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Loan"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Payable"),
                 Text("TZS 120,000"),
-              ]),
-          Row(
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text("Profit & Loss"),
                 Text("TZS 120,000"),
-              ]),
-          const Divider(),
-          Row(
+              ],
+            ),
+            const Divider(),
+            Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Text(
@@ -329,7 +362,9 @@ Widget _balanceSheet() => Card(
                   "TZS 120,000",
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
-              ])
-        ],
+              ],
+            )
+          ],
+        ),
       ),
-    ));
+    );
