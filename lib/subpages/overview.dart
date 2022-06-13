68eb9256681460b92ad265b66da2f1edd5ef9be1
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-06-14 00:15:36 +0300

    ---

--- lib/subpages/overview.dart
+++ lib/subpages/overview.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:patoapp/components/themeData.dart';
-// import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:fl_chart/fl_chart.dart';
 
 class OverviewDialog extends StatelessWidget {
   const OverviewDialog({Key? key}) : super(key: key);
@@ -29,6 +29,25 @@ class OverviewDialog extends StatelessWidget {
         child: ListView(
           children: [
             // _barChartOverview(),
+            Card(
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text('Reports'),
+                      Icon(Icons.arrow_forward_ios,
+                          color: Colors.black38, size: 14),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            _firstRow(),
+            const BarChartSample2(),
             _expenses(),
             _invoices(),
             _payable(),
@@ -38,6 +57,87 @@ class OverviewDialog extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  _firstRow() {
+    return Row(
+      children: [
+        Expanded(
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              child: Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(color: patoGreen, width: 5),
+                  ),
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Sales",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          height: 10,
+                        ),
+                        const Text("TZS 1,300,000"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: SizedBox(
+            height: 100,
+            child: Card(
+              child: Container(
+                decoration: const BoxDecoration(
+                  border: Border(
+                    left: BorderSide(color: patoRed, width: 5),
+                  ),
+                ),
+                child: InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Purchases",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          height: 10,
+                        ),
+                        const Text("TZS 650,000"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -368,3 +468,418 @@ Widget _balanceSheet() => Card(
         ),
       ),
     );
+
+class BarChartSample2 extends StatefulWidget {
+  const BarChartSample2({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => BarChartSample2State();
+}
+
+class BarChartSample2State extends State<BarChartSample2> {
+  final Color leftBarColor = patoPrimaryColor;
+  final Color rightBarColor = const Color(0xffff5182);
+  final double width = 7;
+
+  late List<BarChartGroupData> rawBarGroups;
+  late List<BarChartGroupData> showingBarGroups;
+
+  int touchedGroupIndex = -1;
+
+  @override
+  void initState() {
+    super.initState();
+    final barGroup1 = makeGroupData(0, 5, 12);
+    final barGroup2 = makeGroupData(1, 16, 12);
+    final barGroup3 = makeGroupData(2, 18, 5);
+    final barGroup4 = makeGroupData(3, 20, 16);
+    final barGroup5 = makeGroupData(4, 17, 6);
+    final barGroup6 = makeGroupData(5, 19, 1.5);
+    final barGroup7 = makeGroupData(6, 10, 1.5);
+
+    final items = [
+      barGroup1,
+      barGroup2,
+      barGroup3,
+      barGroup4,
+      barGroup5,
+      barGroup6,
+      barGroup7,
+    ];
+
+    rawBarGroups = items;
+
+    showingBarGroups = rawBarGroups;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+        // color: const Color(0xff2c4260),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  // makeTransactionsIcon(),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Cash flow",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text("Past 12 month"),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Tsh 1,320,000",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16),
+                      ),
+                      Text("Cash Balance"),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.brightness_1,
+                              size: 14, color: leftBarColor),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(
+                            'Money in',
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.brightness_1,
+                              size: 14, color: rightBarColor),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text(
+                            'Money out',
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 38,
+              ),
+              Expanded(
+                child: BarChart(
+                  BarChartData(
+                    maxY: 20,
+                    barTouchData: BarTouchData(
+                        touchTooltipData: BarTouchTooltipData(
+                          tooltipBgColor: Colors.grey,
+                          getTooltipItem: (_a, _b, _c, _d) => null,
+                        ),
+                        touchCallback: (FlTouchEvent event, response) {
+                          if (response == null || response.spot == null) {
+                            setState(() {
+                              touchedGroupIndex = -1;
+                              showingBarGroups = List.of(rawBarGroups);
+                            });
+                            return;
+                          }
+
+                          touchedGroupIndex =
+                              response.spot!.touchedBarGroupIndex;
+
+                          setState(() {
+                            if (!event.isInterestedForInteractions) {
+                              touchedGroupIndex = -1;
+                              showingBarGroups = List.of(rawBarGroups);
+                              return;
+                            }
+                            showingBarGroups = List.of(rawBarGroups);
+                            if (touchedGroupIndex != -1) {
+                              var sum = 0.0;
+                              for (var rod
+                                  in showingBarGroups[touchedGroupIndex]
+                                      .barRods) {
+                                sum += rod.toY;
+                              }
+                              final avg = sum /
+                                  showingBarGroups[touchedGroupIndex]
+                                      .barRods
+                                      .length;
+
+                              showingBarGroups[touchedGroupIndex] =
+                                  showingBarGroups[touchedGroupIndex].copyWith(
+                                barRods: showingBarGroups[touchedGroupIndex]
+                                    .barRods
+                                    .map((rod) {
+                                  return rod.copyWith(toY: avg);
+                                }).toList(),
+                              );
+                            }
+                          });
+                        }),
+                    titlesData: FlTitlesData(
+                      show: true,
+                      rightTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      topTitles: AxisTitles(
+                        sideTitles: SideTitles(showTitles: false),
+                      ),
+                      bottomTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          getTitlesWidget: bottomTitles,
+                          reservedSize: 42,
+                        ),
+                      ),
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 28,
+                          interval: 1,
+                          getTitlesWidget: leftTitles,
+                        ),
+                      ),
+                    ),
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    barGroups: showingBarGroups,
+                    gridData: FlGridData(show: false),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 38,
+              ),
+              const Text(
+                "Your next month looks critical",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "You will probably increase earning by Tsh 80,000 you need for your daily operations",
+                // maxLines: 10,
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  border: Border.all(color: Colors.black),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Forcasted Income",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 4,
+                            ),
+                            Text("March, 01 - 20, 2022"),
+                          ]),
+                      const Text(
+                        "Tsh 780,000",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget leftTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff7589a2),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    if (value == 0) {
+      text = '1K';
+    } else if (value == 10) {
+      text = '5K';
+    } else if (value == 19) {
+      text = '10K';
+    } else {
+      return Container();
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 0,
+      child: Text(text, style: style),
+    );
+  }
+
+  Widget bottomTitles(double value, TitleMeta meta) {
+    const style = TextStyle(
+      color: Color(0xff7589a2),
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 0:
+        text = const Text(
+          'Mn',
+          style: style,
+        );
+        break;
+      case 1:
+        text = const Text(
+          'Te',
+          style: style,
+        );
+        break;
+      case 2:
+        text = const Text(
+          'Wd',
+          style: style,
+        );
+        break;
+      case 3:
+        text = const Text(
+          'Tu',
+          style: style,
+        );
+        break;
+      case 4:
+        text = const Text(
+          'Fr',
+          style: style,
+        );
+        break;
+      case 5:
+        text = const Text(
+          'St',
+          style: style,
+        );
+        break;
+      case 6:
+        text = const Text(
+          'Sn',
+          style: style,
+        );
+        break;
+      default:
+        text = const Text(
+          '',
+          style: style,
+        );
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16,
+      child: text,
+    );
+  }
+
+  BarChartGroupData makeGroupData(int x, double y1, double y2) {
+    return BarChartGroupData(barsSpace: 4, x: x, barRods: [
+      BarChartRodData(
+        toY: y1,
+        color: leftBarColor,
+        width: width,
+      ),
+      BarChartRodData(
+        toY: y2,
+        color: rightBarColor,
+        width: width,
+      ),
+    ]);
+  }
+
+  // Widget makeTransactionsIcon() {
+  //   const width = 4.5;
+  //   const space = 3.5;
+  //   return Row(
+  //     mainAxisSize: MainAxisSize.min,
+  //     crossAxisAlignment: CrossAxisAlignment.center,
+  //     children: <Widget>[
+  //       Container(
+  //         width: width,
+  //         height: 10,
+  //         color: Colors.white.withOpacity(0.4),
+  //       ),
+  //       const SizedBox(
+  //         width: space,
+  //       ),
+  //       Container(
+  //         width: width,
+  //         height: 28,
+  //         color: Colors.white.withOpacity(0.8),
+  //       ),
+  //       const SizedBox(
+  //         width: space,
+  //       ),
+  //       Container(
+  //         width: width,
+  //         height: 42,
+  //         color: Colors.white.withOpacity(1),
+  //       ),
+  //       const SizedBox(
+  //         width: space,
+  //       ),
+  //       Container(
+  //         width: width,
+  //         height: 28,
+  //         color: Colors.white.withOpacity(0.8),
+  //       ),
+  //       const SizedBox(
+  //         width: space,
+  //       ),
+  //       Container(
+  //         width: width,
+  //         height: 10,
+  //         color: Colors.white.withOpacity(0.4),
+  //       ),
+  //     ],
+  //   );
+  // }
+
+}
