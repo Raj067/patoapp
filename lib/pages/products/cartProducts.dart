4c976170ac9d7acca387997a8a3ce14143c6c43d
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-06-14 12:58:20 +0300

    ---

--- lib/pages/products/cartProducts.dart
+++ lib/pages/products/cartProducts.dart
@@ -11,10 +11,9 @@ class ProductsCart extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Cart',
+          'Add Transaction',
           style: TextStyle(color: Colors.white),
         ),
-        centerTitle: true,
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -25,8 +24,70 @@ class ProductsCart extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text("Cart Page"),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(children: [
+                _allSelectedProducts(context),
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _allSelectedProducts(BuildContext context) {
+    List<Widget> data = [];
+    for (var element in products) {
+      if (element.addedToCart > 0) {
+        data.add(_signleSelectedProduct(context, element));
+      }
+    }
+    return Column(children: data);
+  }
+
+  _signleSelectedProduct(BuildContext context, SingleProduct product) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  product.productName,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Tsh ${product.getTotalPrice()}",
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Container(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Item Subtotal:",
+                  style: TextStyle(fontSize: 12),
+                ),
+                Text(
+                  "${product.addedToCart} canns x Tsh ${product.productPrice} = Tsh ${product.getTotalPrice()}",
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
