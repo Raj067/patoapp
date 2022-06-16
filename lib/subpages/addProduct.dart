f067e714b336bd55dbe2ae1dd7b93208dfb71ce1
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-06-16 11:34:08 +0300

    ---

--- lib/subpages/addProduct.dart
+++ lib/subpages/addProduct.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:patoapp/components/themeData.dart';
 
-class AddProductPage extends StatelessWidget {
+class AddProductPage extends StatefulWidget {
   const AddProductPage({Key? key}) : super(key: key);
 
+  @override
+  State<AddProductPage> createState() => _AddProductPageState();
+}
+
+class _AddProductPageState extends State<AddProductPage> {
+  bool _isSupplierActivated = false;
+  int _value = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Add Product',
+          'Add Item',
           style: TextStyle(color: Colors.white),
         ),
         // centerTitle: true,
@@ -23,8 +30,81 @@ class AddProductPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      _value = 1;
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Radio(
+                        activeColor: patoPrimaryColor,
+                        value: 1,
+                        groupValue: _value,
+                        onChanged: (val) {
+                          setState(() {
+                            _value = 1;
+                          });
+                        },
+                      ),
+                      Container(
+                        width: 5,
+                      ),
+                      const Text("Product"),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      _value = 2;
+                    });
+                  },
+                  child: Row(
+                    children: [
+                      Radio(
+                        activeColor: patoPrimaryColor,
+                        value: 2,
+                        groupValue: _value,
+                        onChanged: (val) {
+                          setState(() {
+                            _value = 2;
+                          });
+                        },
+                      ),
+                      Container(
+                        width: 5,
+                      ),
+                      const Text("Service"),
+                      Container(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          const Divider(height: 0),
+          _value == 1 ? _addProduct() : _addService(),
+        ],
+      ),
+    );
+  }
+
+  _addProduct() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
         child: ListView(
           children: [
             // _formField1(),
@@ -58,6 +138,8 @@ class AddProductPage extends StatelessWidget {
                 ),
               ),
             ),
+            Container(height: 10),
+            const Text("Pricing & Other Details"),
             Container(height: 10),
             SizedBox(
               height: 45,
@@ -99,6 +181,7 @@ class AddProductPage extends StatelessWidget {
                     height: 45,
                     child: TextFormField(
                       decoration: const InputDecoration(
+                        suffixIcon: Icon(Icons.question_mark_outlined),
                         label: Text("Stock Level"),
                         border: OutlineInputBorder(),
                       ),
@@ -108,54 +191,131 @@ class AddProductPage extends StatelessWidget {
               ],
             ),
             Container(height: 10),
-            SizedBox(
-              height: 45,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  label: Text("Name"),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            Container(height: 10),
-            SizedBox(
-              height: 45,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  label: Text("Phone Number"),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ),
-            Container(height: 10),
-            SizedBox(
-              height: 45,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  label: Text("Email"),
-                  border: OutlineInputBorder(),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              const Text("Supplier Contact"),
+              Switch(
+                  activeColor: patoPrimaryColor,
+                  value: _isSupplierActivated,
+                  onChanged: (val) {
+                    setState(() {
+                      _isSupplierActivated = val;
+                    });
+                  })
+            ]),
+
+            _isSupplierActivated
+                ? Column(
+                    children: [
+                      Container(height: 10),
+                      SizedBox(
+                        height: 45,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text("Name"),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Container(height: 10),
+                      SizedBox(
+                        height: 45,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text("Phone Number"),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Container(height: 10),
+                      SizedBox(
+                        height: 45,
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text("Email"),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Container(height: 10)
+                    ],
+                  )
+                : Container(height: 10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Add Product",
+                  style: TextStyle(color: patoWhite),
                 ),
               ),
             ),
           ],
         ),
       ),
-      persistentFooterButtons: [
-        TextButton(
-          child: const Text("Add Product"),
-          onPressed: () {},
-        ),
-      ],
     );
   }
 
-  _formField1() {
-    return SizedBox(
-      height: 45,
-      child: TextFormField(
-        decoration: const InputDecoration(
-          label: Text("hello"),
-          border: OutlineInputBorder(),
+  _addService() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: ListView(
+          children: [
+            // _formField1(),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Service Name"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Service Charge"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Service Unit"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Description"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Add Service",
+                  style: TextStyle(color: patoWhite),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/subpages/addTransaction.dart
+++ lib/subpages/addTransaction.dart
@@ -96,43 +96,385 @@ class _AddTransactionDialogState extends State<AddTransactionDialog> {
             ],
           ),
           const Divider(height: 0),
-          Table(
-            border: TableBorder.all(width: 1.0, color: Colors.grey),
-            children: [
-              TableRow(children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Text("Invoice No 1"),
+          _value == 1 ? _addSales() : _addExpenses(),
+        ],
+      ),
+    );
+  }
+
+  _addSales() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: ListView(
+          children: [
+            Table(
+              border: TableBorder.all(width: 1.0, color: Colors.grey),
+              children: [
+                TableRow(children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text("Invoice No 1"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Date: ${DateFormat("dd-MM-yyyy").format(DateTime.now())}",
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+            const Divider(height: 0),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Amount"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Party Name"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            Card(
+              color: patoPrimaryColor,
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'Add Items to sale',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios,
+                          color: Colors.white, size: 14),
+                    ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Center(
-                    child: Text(
-                      "Date: ${DateFormat("dd-MM-yyyy").format(DateTime.now())}",
+              ),
+            ),
+            _discount(),
+            Container(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Total Amount",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Tsh: totalAmount",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Container(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Balance due",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: patoPrimaryColor),
+                ),
+                Text(
+                  "Tsh: balanceDue",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: patoPrimaryColor),
+                ),
+              ],
+            ),
+            Container(height: 10),
+            SizedBox(
+              // height: 180,
+              child: TextFormField(
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                minLines: 1,
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  label: Text("Descriptions"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Add Transaction",
+                  style: TextStyle(color: patoWhite),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _addExpenses() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        child: ListView(
+          children: [
+            Table(
+              border: TableBorder.all(width: 1.0, color: Colors.grey),
+              children: [
+                TableRow(children: [
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text("Bill No 1"),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Date: ${DateFormat("dd-MM-yyyy").format(DateTime.now())}",
+                      ),
+                    ),
+                  ),
+                ]),
+              ],
+            ),
+            const Divider(height: 0),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Amount"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            const Text("Other Informations:"),
+            Card(
+              color: patoPrimaryColor,
+              child: InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        'Add Items to Purchases',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios,
+                          color: Colors.white, size: 14),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Expenses Category"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            SizedBox(
+              height: 45,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  label: Text("Add Contact"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Total Amount",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Tsh: totalAmount",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Container(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      Checkbox(
+                        value: true,
+                        onChanged: (val) {},
+                        activeColor: patoPrimaryColor,
+                      ),
+                      const Text(
+                        "Paid Amount",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: SizedBox(
+                    height: 45,
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Tsh"),
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
                 ),
-              ]),
-            ],
-          ),
-          const Divider(height: 0),
-          Center(
-            child: Text("$_value"),
-          )
-        ],
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(10),
-        child: ElevatedButton(
-          child: const Text(
-            "Add Transaction",
-            style: TextStyle(color: Colors.white),
-          ),
-          onPressed: () {},
+              ],
+            ),
+            Container(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "Balance due",
+                  style: TextStyle(fontWeight: FontWeight.bold, color: patoRed),
+                ),
+                Text(
+                  "Tsh: balanceDue",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: patoPrimaryColor),
+                ),
+              ],
+            ),
+            Container(height: 10),
+            SizedBox(
+              // height: 180,
+              child: TextFormField(
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.newline,
+                minLines: 1,
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  label: Text("Descriptions"),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(height: 10),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  "Add Transaction",
+                  style: TextStyle(color: patoWhite),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  _discount() {
+    return Column(
+      children: [
+        Container(height: 15),
+        const Divider(height: 0),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(14, 5, 15, 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Discount",
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+              Container(width: 5),
+              Expanded(
+                child: Container(
+                  height: 35,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: Container(
+                        color: const Color.fromARGB(255, 255, 200, 200),
+                        child: const Icon(
+                          Icons.percent,
+                          color: patoRed,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Container(width: 10),
+              Expanded(
+                child: SizedBox(
+                  height: 35,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      prefixIcon: Container(
+                        color: Colors.grey,
+                        child: const Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text("Tsh",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(height: 0),
+        Container(height: 15),
+      ],
     );
   }
 }
