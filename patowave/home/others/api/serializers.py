1b8f984a24b557567b206cb834e39912d66b6a9a
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-07-02 08:17:50 +0300

    ---

--- patowave/home/others/api/serializers.py
+++ patowave/home/others/api/serializers.py
@@ -21,9 +21,23 @@ class PaymentSerializer(serializers.ModelSerializer):
         fields = ('id', 'amount', 'is_payment_in',)
 
 
+class InvoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invoice
+        fields = ('id', 'amount', 'is_payment_in',)
+
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchase
+        fields = ('id', 'amount', 'is_payment_in',)
+
+
 class CustomerSerializer(serializers.ModelSerializer):
     customer_payment = PaymentSerializer(many=True, read_only=True)
- 
+    customer_invoice = PaymentSerializer(many=True, read_only=True)
+    customer_purchase = PaymentSerializer(many=True, read_only=True)
     class Meta:
         model = Customer
-        fields = ('id', 'customer_name', 'customer_number', 'customer_payment')
+        fields = ('id', 'customer_name', 'customer_number',
+                  'customer_payment', 'customer_invoice', 'customer_purchase')
