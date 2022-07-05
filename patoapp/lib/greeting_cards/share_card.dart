cafd3fbe065ac16284cb749e6c3d66bbb28af469
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-07-05 16:19:45 +0300

    ---

---
+++ patoapp/lib/greeting_cards/share_card.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'package:patoapp/api/apis.dart';
+import 'package:patoapp/data/greeting_card.dart';
+import 'package:patoapp/themes/light_theme.dart';
+
+class ShareGreetingCard extends StatelessWidget {
+  final SingleGreetingCard myCard;
+  const ShareGreetingCard({Key? key, required this.myCard}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Share card',
+          style: TextStyle(color: Colors.white),
+        ),
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            color: patowaveWhite,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+            child: Card(
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+              elevation: 0,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Image.network(
+                      "$imageBaseUrl${myCard.greetingCard}",
+                    ),
+                    Container(height: 15),
+                    SizedBox(
+                      child: TextFormField(
+                        initialValue: myCard.description,
+                        keyboardType: TextInputType.multiline,
+                        textInputAction: TextInputAction.newline,
+                        minLines: 6,
+                        maxLines: null,
+                        decoration: const InputDecoration(
+                          label: Text(
+                            "Text Message",
+                            style: TextStyle(
+                                fontStyle: FontStyle.italic, fontSize: 14),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(height: 10),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      persistentFooterButtons: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Expanded(
+            child: ElevatedButton(
+              style: ButtonStyle(
+                // MaterialStateProperty<Color?>? backgroundColor,
+                backgroundColor: MaterialStateProperty.all(patowaveErrorRed),
+                minimumSize: MaterialStateProperty.all(
+                  const Size(45, 45),
+                ),
+                shape: MaterialStateProperty.all(
+                  const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                "Cancel",
+              ),
+            ),
+          ),
+          Container(width: 10),
+          Expanded(
+            child: ElevatedButton(
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all(
+                  const Size(45, 45),
+                ),
+                shape: MaterialStateProperty.all(
+                  const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text(
+                "Send",
+              ),
+            ),
+          ),
+        ]),
+      ],
+    );
+  }
+}
--- patoapp/lib/more/greetings.dart
+++ patoapp/lib/more/greetings.dart
@@ -1,11 +1,73 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:patoapp/greeting_cards/good_morning.dart';
-import 'package:patoapp/greeting_cards/offers.dart';
-import 'package:patoapp/greeting_cards/thank_you.dart';
+import 'package:patoapp/api/apis.dart';
+import 'package:patoapp/data/greeting_card.dart';
+import 'package:patoapp/greeting_cards/share_card.dart';
 import 'package:patoapp/themes/light_theme.dart';
+import 'package:http/http.dart' as http;
 
-class MainGreetingsCards extends StatelessWidget {
+class MainGreetingsCards extends StatefulWidget {
   const MainGreetingsCards({Key? key}) : super(key: key);
+
+  @override
+  State<MainGreetingsCards> createState() => _MainGreetingsCardsState();
+}
+
+class _MainGreetingsCardsState extends State<MainGreetingsCards> {
+  List thankYouData = [];
+  List offersData = [];
+  List goodMorningData = [];
+  bool isLoading = true;
+  fetchData() async {
+    var cardData = await http.get(
+      Uri.parse(
+        "${baseUrl}api/greeting-cards/",
+      ),
+      headers: authHeaders,
+    );
+    if (cardData.statusCode == 200) {
+      List<SingleGreetingCard> newDataThankYou = [];
+      List<SingleGreetingCard> newDataOffers = [];
+      List<SingleGreetingCard> newDataGoodMorning = [];
+      for (var element in jsonDecode(cardData.body)) {
+        if (element['card_category'] == 'thank-you') {
+          newDataThankYou.add(SingleGreetingCard(
+            cardCategory: element['card_category'],
+            greetingCard: element['greeting_card'],
+            description: element['default_text'],
+            id: element['id'],
+          ));
+        }
+        if (element['card_category'] == 'offers') {
+          newDataOffers.add(SingleGreetingCard(
+            cardCategory: element['card_category'],
+            greetingCard: element['greeting_card'],
+            description: element['default_text'],
+            id: element['id'],
+          ));
+        }
+        if (element['card_category'] == 'good-morning') {
+          newDataGoodMorning.add(SingleGreetingCard(
+            cardCategory: element['card_category'],
+            greetingCard: element['greeting_card'],
+            description: element['default_text'],
+            id: element['id'],
+          ));
+        }
+      }
+      thankYouData = newDataThankYou;
+      offersData = newDataOffers;
+      goodMorningData = newDataGoodMorning;
+      isLoading = false;
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,12 +104,173 @@ class MainGreetingsCards extends StatelessWidget {
             ],
           ),
         ),
-        body: const TabBarView(
+        body: TabBarView(
           children: <Widget>[
-            ThankYouCards(),
-            OfferCards(),
-            GoodMorningCards(),
+            _thankYou(),
+            _offers(),
+            _goodMorning(),
           ],
+        ),
+      ),
+    );
+  }
+
+  _thankYou() {
+    return isLoading
+        ? const SizedBox(
+            height: 100,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : _loadedThankYouCard();
+  }
+
+  _offers() {
+    return isLoading
+        ? const SizedBox(
+            height: 100,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : _loadedOffersCard();
+  }
+
+  _goodMorning() {
+    return isLoading
+        ? const SizedBox(
+            height: 100,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : _loadedGoodMorningCard();
+  }
+
+  _loadedThankYouCard() {
+    List<Widget> myCustomData = [];
+    for (SingleGreetingCard element in thankYouData) {
+      myCustomData.add(_singleCard(
+        element,
+        cardImage: element.greetingCard,
+        defaultText: element.description,
+        id: element.id,
+      ));
+    }
+    return ListView(
+      children: myCustomData,
+    );
+  }
+
+  _loadedOffersCard() {
+    List<Widget> myCustomData = [];
+    for (SingleGreetingCard element in offersData) {
+      myCustomData.add(_singleCard(
+        element,
+        cardImage: element.greetingCard,
+        defaultText: element.description,
+        id: element.id,
+      ));
+    }
+    return ListView(
+      children: myCustomData,
+    );
+  }
+
+  _loadedGoodMorningCard() {
+    List<Widget> myCustomData = [];
+    for (SingleGreetingCard element in goodMorningData) {
+      myCustomData.add(_singleCard(
+        element,
+        cardImage: element.greetingCard,
+        defaultText: element.description,
+        id: element.id,
+      ));
+    }
+    return ListView(
+      children: myCustomData,
+    );
+  }
+
+  _singleCard(
+    SingleGreetingCard myCard, {
+    required cardImage,
+    required defaultText,
+    required id,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(15),
+          ),
+        ),
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Image.network(
+                "$imageBaseUrl$cardImage",
+              ),
+              Container(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        const Icon(Icons.download, size: 18),
+                        Container(width: 10),
+                        const Text("Download"),
+                      ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) =>
+                              ShareGreetingCard(myCard: myCard),
+                          fullscreenDialog: true,
+                        ),
+                      );
+                    },
+                    child: Row(
+                      children: [
+                        const Icon(Icons.share, size: 18),
+                        Container(width: 10),
+                        const Text("Share"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Container(height: 10),
+            ],
+          ),
         ),
       ),
     );
