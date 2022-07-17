9a2390884127ca8d61cbaec01552c6edfa0a1e64
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-07-17 21:04:52 +0300

    ---

---
+++ patosite/conf/config.py
@@ -0,0 +1,10 @@
+import multiprocessing
+
+command = '/rajabu/env/bin/gunicorn'
+pythonpath = '/rajabu/patoapp/patosite'
+bind = '0.0.0.0:8000'
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'tornado'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
