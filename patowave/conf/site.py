e48007bf9806185d51d7181c07a645230c8c05ac
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-08-30 15:25:14 +0300

    ---

--- patowave/conf/site.py
+++ patowave/conf/site.py
@@ -1,8 +1,8 @@
 import multiprocessing
 
 command = '/rajabu/env/bin/gunicorn'
-pythonpath = '/rajabu/patoapp/patosite'
-bind = '0.0.0.0:8000'
+pythonpath = '/rajabu/patoapp/patowave'
+bind = '0.0.0.0:9000'
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'tornado'
 worker_connections = 1000
